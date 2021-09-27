84c1881f46c2906a5144d2ca69e53a243d784a26
Ali Al-Timimi <AliAl-Timimi@users.noreply.github.com>
2021-09-27 22:00:03 +0200

    Finalized enum

--- CA/LanguageType.cs
+++ CA/LanguageType.cs
@@ -2,6 +2,10 @@ namespace CA
 {
     public enum LanguageType
     {
-        
+        Ppl, // Procedural Programming Language
+        Fpl, // Functional Programming Language
+        Spl, // Scripting Programming Language
+        Lpl, // Logic Programming Language
+        Oopl // Object-Oriented Programming Language
     }
 }
