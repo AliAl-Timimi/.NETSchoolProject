4a2cfa466dbaf573bd8263e3ed1359fdeac990de
Ali Al-Timimi <AliAl-Timimi@users.noreply.github.com>
2021-10-20 17:31:49 +0200

    changed Program.cs inner methods to private class methods, renamed Program.cs in domain to Software.cs

--- UI.CA/Program.cs
+++ UI.CA/Program.cs
@@ -7,29 +7,34 @@ namespace Languages.UI.CA
 {
     class Program
     {
+        private IManager _manager = new Manager();
+        private int _input;
+        private int _input2;
+        private bool _cont;
         static void Main(string[] args)
         {
-            IManager manager = new Manager();
-            int input;
-            int input2;
-            bool cont;
+            Program program = new Program();
+            program.Run();
+        }
 
+        private void Run()
+        {
             do
             {
                 PrintMenu();
                 do
                 {
                     Console.Write("Choice (1-6): ");
-                    cont = int.TryParse(Console.ReadLine(), out input) && input >= 0 && input < 7;
-                    if (!cont)
+                    _cont = int.TryParse(Console.ReadLine(), out _input) && _input >= 0 && _input < 7;
+                    if (!_cont)
                     {
                         Console.WriteLine("Ongeldige invoer!");
                     }
 
                     Console.WriteLine();
-                } while (!cont);
+                } while (!_cont);
 
-                switch (input)
+                switch (_input)
                 {
                     case 1:
                         PrintLanguages();
@@ -52,166 +57,167 @@ namespace Languages.UI.CA
                 }
 
                 Console.WriteLine();
-            } while (input != 0);
+            } while (_input != 0);
+        }
 
-            void PrintMenu()
+        private void PrintMenu()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("==========================");
+            Console.WriteLine("0) Quit");
+            Console.WriteLine("1) Show all Languages");
+            Console.WriteLine("2) Show languages by type");
+            Console.WriteLine("3) Show all IDEs");
+            Console.WriteLine("4) Show IDEs with name and/or year of release");
+            Console.WriteLine("5) Add a Language");
+            Console.WriteLine("6) Add an IDE");
+        }
+
+        private void PrintLanguages()
+        {
+            Console.WriteLine("All languages");
+            Console.WriteLine("=============");
+            foreach (Language language in _manager.GetAllLanguages())
             {
-                Console.WriteLine("What would you like to do?");
-                Console.WriteLine("==========================");
-                Console.WriteLine("0) Quit");
-                Console.WriteLine("1) Show all Languages");
-                Console.WriteLine("2) Show languages by type");
-                Console.WriteLine("3) Show all IDEs");
-                Console.WriteLine("4) Show IDEs with name and/or year of release");
-                Console.WriteLine("5) Add a Language");
-                Console.WriteLine("6) Add an IDE");
+                Console.WriteLine(language);
             }
+        }
 
-            void PrintLanguages()
+        private void PrintIdes()
+        {
+            Console.WriteLine("All IDEs");
+            Console.WriteLine("========");
+            foreach (IDE ide in _manager.GetAllIdes())
             {
-                Console.WriteLine("All languages");
-                Console.WriteLine("=============");
-                foreach (Language language in manager.GetAllLanguages())
-                {
-                    Console.WriteLine(language);
-                }
+                Console.WriteLine(ide);
             }
+        }
 
-            void PrintIdes()
+        private void LanguageByGenre()
+        {
+            do
             {
-                Console.WriteLine("All IDEs");
-                Console.WriteLine("========");
-                foreach (IDE ide in manager.GetAllIdes())
-                {
-                    Console.WriteLine(ide);
-                }
-            }
-
-            void LanguageByGenre()
-            {
-                do
-                {
-                    PrintEnum();
-                    cont = int.TryParse(Console.ReadLine(), out input2) && input2 > 0 &&
-                           input2 <= Enum.GetValues(typeof(LanguageType)).Length;
-                    if (!cont)
-                        Console.WriteLine("Ongeldige invoer!");
-                } while (!cont);
-
-                foreach (Language lang in manager.GetLanguageByGenre(Enum.GetName(typeof(LanguageType), input2 - 1)))
-                {
-                    Console.WriteLine(lang);
-                }
-            }
-
-            void PrintEnum()
-            {
-                Console.Write("Language type (");
-                for (int i = 0; i < Enum.GetValues(typeof(LanguageType)).Length; i++)
-                {
-                    Console.Write($"{i + 1}={Enum.GetName(typeof(LanguageType), i)}");
-                    if (i != Enum.GetValues(typeof(LanguageType)).Length - 1)
-                        Console.Write(", ");
-                }
-
-                Console.Write("): ");
-            }
-
-            void IdeByNameAndYear()
-            {
-                Console.Write("Enter (part of) a name or leave blank: ");
-                string search = Console.ReadLine();
-                string yearinput;
-                int year = 0;
-                do
-                {
-                    Console.Write("Enter a year or leave blank: ");
-                    yearinput = Console.ReadLine();
-                    if (!string.IsNullOrEmpty(yearinput))
-                    {
-                        cont = int.TryParse(yearinput, out year) && year <= DateTime.Now.Year;
-                        if (!cont)
-                        {
-                            Console.WriteLine(year + "\nOngeldige invoer!");
-                        }
-                    }
-                    else
-                        cont = true;
-                } while (!cont);
-
-                foreach (IDE ide in manager.GetIdeByNameAndReleaseYear(search, year))
-                {
-                    Console.WriteLine(ide);
-                }
-            }
-
-            void AddLanguage()
-            {
-                Console.WriteLine("Add Language");
-                Console.WriteLine("=======");
-                
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                
                 PrintEnum();
-                int.TryParse(Console.ReadLine(), out int typeInt);
-                if (typeInt > Enum.GetValues(typeof(LanguageType)).Length)
-                {
-                    typeInt = -1;
-                }
-                LanguageType type = (LanguageType) typeInt - 1;
-                
-                Console.Write("Release date (yyyy/mm/dd): ");
-                DateTime.TryParse(Console.ReadLine(), out DateTime release);
-                
-                Console.Write("Version: ");
-                double.TryParse(Console.ReadLine(), out double version);
-                
-                try
-                {
-                    manager.AddLanguage(name, type, release, version);
-                }
-                catch (ValidationException e)
-                {
-                    Console.WriteLine("Error: "+e.Message + "\nPlease try again...\n");
-                    AddLanguage();
-                }
+                _cont = int.TryParse(Console.ReadLine(), out _input2) && _input2 > 0 &&
+                       _input2 <= Enum.GetValues(typeof(LanguageType)).Length;
+                if (!_cont)
+                    Console.WriteLine("Ongeldige invoer!");
+            } while (!_cont);
+
+            foreach (Language lang in _manager.GetLanguageByGenre(Enum.GetName(typeof(LanguageType), _input2 - 1)))
+            {
+                Console.WriteLine(lang);
+            }
+        }
+
+        private void PrintEnum()
+        {
+            Console.Write("Language type (");
+            for (int i = 0; i < Enum.GetValues(typeof(LanguageType)).Length; i++)
+            {
+                Console.Write($"{i + 1}={Enum.GetName(typeof(LanguageType), i)}");
+                if (i != Enum.GetValues(typeof(LanguageType)).Length - 1)
+                    Console.Write(", ");
             }
 
-            void AddIde()
+            Console.Write("): ");
+        }
+
+        private void IdeByNameAndYear()
+        {
+            Console.Write("Enter (part of) a name or leave blank: ");
+            string search = Console.ReadLine();
+            string yearinput;
+            int year = 0;
+            do
             {
-                Console.WriteLine("Add IDE");
-                Console.WriteLine("=======");
-                
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                
-                Console.Write("Manufacturer: ");
-                string manufacturer = Console.ReadLine();
-                
-                Console.Write("Release date (yyyy/mm/dd): ");
-                DateTime.TryParse(Console.ReadLine(), out DateTime release);
-                
-                Console.Write("Amount of supported languages: ");
-                string amountSup = Console.ReadLine();
-                int supportedLanguages;
-                if (!string.IsNullOrEmpty(amountSup))
-                    int.TryParse(amountSup, out supportedLanguages);
+                Console.Write("Enter a year or leave blank: ");
+                yearinput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(yearinput))
+                {
+                    _cont = int.TryParse(yearinput, out year) && year <= DateTime.Now.Year;
+                    if (!_cont)
+                    {
+                        Console.WriteLine(year + "\nOngeldige invoer!");
+                    }
+                }
                 else
-                    supportedLanguages = -1;
-                
-                Console.Write("Price: ");
-                double.TryParse(Console.ReadLine(), out double price);
-                
-                try
-                {
-                    manager.AddIde(name, manufacturer, release, supportedLanguages, price);
-                }
-                catch (ValidationException e)
-                {
-                    Console.WriteLine("Error: " + e.Message + "\nPlease try again...\n");
-                    AddIde();
-                }
+                    _cont = true;
+            } while (!_cont);
+
+            foreach (IDE ide in _manager.GetIdeByNameAndReleaseYear(search, year))
+            {
+                Console.WriteLine(ide);
+            }
+        }
+
+        private void AddLanguage()
+        {
+            Console.WriteLine("Add Language");
+            Console.WriteLine("=======");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            PrintEnum();
+            int.TryParse(Console.ReadLine(), out int typeInt);
+            if (typeInt > Enum.GetValues(typeof(LanguageType)).Length)
+            {
+                typeInt = -1;
+            }
+
+            LanguageType type = (LanguageType) typeInt - 1;
+
+            Console.Write("Release date (yyyy/mm/dd): ");
+            DateTime.TryParse(Console.ReadLine(), out DateTime release);
+
+            Console.Write("Version: ");
+            double.TryParse(Console.ReadLine(), out double version);
+
+            try
+            {
+                _manager.AddLanguage(name, type, release, version);
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine("Error: " + e.Message + "\nPlease try again...\n");
+                AddLanguage();
+            }
+        }
+
+        private void AddIde()
+        {
+            Console.WriteLine("Add IDE");
+            Console.WriteLine("=======");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Manufacturer: ");
+            string manufacturer = Console.ReadLine();
+
+            Console.Write("Release date (yyyy/mm/dd): ");
+            DateTime.TryParse(Console.ReadLine(), out DateTime release);
+
+            Console.Write("Amount of supported languages: ");
+            string amountSup = Console.ReadLine();
+            int supportedLanguages;
+            if (!string.IsNullOrEmpty(amountSup))
+                int.TryParse(amountSup, out supportedLanguages);
+            else
+                supportedLanguages = -1;
+
+            Console.Write("Price: ");
+            double.TryParse(Console.ReadLine(), out double price);
+
+            try
+            {
+                _manager.AddIde(name, manufacturer, release, supportedLanguages, price);
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine("Error: " + e.Message + "\nPlease try again...\n");
+                AddIde();
             }
         }
     }
